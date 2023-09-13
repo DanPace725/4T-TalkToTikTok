faf55f3a5b98d4b3376b56674984b6f0f0f1130a
DanPace725 <daniel.a.pace725@gmail.com>
2023-09-12 19:33:14 -0700

    add transcription code

---
+++ 4T/src/transcription.py
@@ -0,0 +1,20 @@
+# transcription.py
+import os
+from audio_util import convert_to_audio
+from openai_util import transcribe_audio 
+from openai_util import correct_transcription
+
+def transcribe_video(video_filename):
+  audio_filename = f"{video_filename}_audio.mp3"
+  
+  success = convert_to_audio(video_filename, audio_filename)
+  if not success:
+    return None
+
+  transcription = transcribe_audio(audio_filename)
+  
+  # Clean up
+  os.remove(audio_filename)
+  
+  return transcription
+
